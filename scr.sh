4c65f4278c24eeb31120cef096292accf3cd4c1f
alex-jigsaw12 <39372177+alex-jigsaw12@users.noreply.github.com>
2022-12-08 19:35:02 +0100

    f

--- scr.sh
+++ scr.sh
@@ -6,7 +6,7 @@ require "octokit"
 json = File.read(ENV.fetch("GITHUB_EVENT_PATH"))
 event = JSON.parse(json)
 
-github = Octokit::Client.new(access_token: ENV["GITHUB_TOKEN"])
+$github = Octokit::Client.new(access_token: ENV["GITHUB_TOKEN"])
 
 if !ENV["GITHUB_TOKEN"]
   puts "Missing GITHUB_TOKEN"
@@ -18,10 +18,10 @@ if ARGV.empty?
   exit(1)
 end
 
-repo = event["repository"]["full_name"]
+$repo = event["repository"]["full_name"]
 
 if ENV.fetch("GITHUB_EVENT_NAME") == "pull_request"
-  pr_number = event["number"]
+  $pr_number = event["number"]
 else
   pulls = github.pull_requests(repo, state: "open")
 
@@ -52,7 +52,9 @@ def chunker f_in, out_pref, chunksize = 6500
 		  break if fh_out.include? "----------- end diff -----------" or fh_in.eof?
           #break if fh_out.size > (chunksize-line.length) || fh_in.eof?
         end
-       somep fh_out
+        message = fh_out
+        coms = $github.issue_comments($repo, pr_number)
+        $github.add_comment($repo, pr_number, message)
 		puts fh_out
 		puts "===================================================================================================================="
       end
@@ -61,13 +63,9 @@ def chunker f_in, out_pref, chunksize = 6500
     end
   end
 
-def somep msg
-
-end
 
 
- message = "msg"
- coms = github.issue_comments(repo, pr_number)
- github.add_comment(repo, pr_number, message)
+
+
 
 chunker "#{file_path}", "output_prefix", 6500
